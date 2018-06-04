21baaf73451edd982a58567b908b9f8aef9e21f9
Damien Irving <irving.damien@gmail.com>
2018-06-04 14:39:43 +1000

    Added function

--- shapes.py
+++ shapes.py
@@ -2,4 +2,15 @@
 
 valid_shapes = [None, None, None, 'triangle', 'square', 'pentagon', 'hexagon']
 
+def shape(sides):
+    """Given a number of sides, tell us the shape.
+    
+    Args:
+      sides (int) : Number of sides
+      
+    Returns:
+      str : The name of the shape
+    
+    """
 
+    return valid_shapes[sides]
